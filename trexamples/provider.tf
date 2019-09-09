68bf8754f6ee58294245fb41b088d32a206b61ac
altaratz <altaratz@gmail.com>
2019-09-09 16:06:44 +0300

    Adding var file

--- trexamples/provider.tf
+++ trexamples/provider.tf
@@ -1,7 +1,25 @@
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secert_key = "${var.aws_secert_key}"
-    region = "us-east-1"
+    region = "${var.region}"
+}
 
+resource "aws_vpc" "main" {
+    cidr_block = "${var.vpc_cidr}"
+    instance_tenancy = "default"
+
+    tags {
+        Name = "main"
+        Location = "Israel"
+    }
+}
+resource "aws_subnet" "Subnet1" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.vpc_subnet_cidr}"
+
+    tags {
+        Name = "subnet1"
+    }
+}
 
 }
---
+++ trexamples/vars.tf
@@ -0,0 +1,9 @@
+variable "region" {
+    default = "us-east-1"
+}
+variable "vpc_cidr" {
+    default = "190.160.0.0/16"
+}
+variable "vpc_subnet_cidr" {
+    default = "190.160.1.0/24"
+}
